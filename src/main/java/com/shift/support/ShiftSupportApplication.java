8e2500e74dc3698985dee44c9e724aebf665d58c
mishima <mishima@keigo-book>
2024-12-28 17:29:36 +0900

    re-init

---
+++ src/main/java/com/shift/support/ShiftSupportApplication.java
@@ -0,0 +1,13 @@
+package com.shift.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shift/support/ShiftSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shift.support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
